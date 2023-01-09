1f81d504a64334fd70596c525466974ae97dc8e9
nizhantiw <nizhantiw@gmail.com>
2023-01-09 21:00:44 +0530

    Updated project JS Project
    Corrected features for project

--- App.js
+++ App.js
@@ -1,125 +1,123 @@
-const todos = document.querySelectorAll(".todo");
-const all_status = document.querySelectorAll(".status");
-let draggableTodo = null;
 
-todos.forEach((todo) => {
-  todo.addEventListener("dragstart", dragStart);
-  todo.addEventListener("dragend", dragEnd);
-});
+let boxes = document.getElementsByClassName('box');
 
-function dragStart() {
-  draggableTodo = this;
-  setTimeout(() => {
-    this.style.display = "none";
-  }, 0);
-  console.log("dragStart");
+document.getElementById('error').innerHTML = "";
+let button = document.getElementById('button');
+button.addEventListener('click', addTask);
+function addTask() {
+    document.getElementById('error').innerHTML = " ";
+    let text = document.getElementById('text').value;
+    //check if text box is empty
+    if (text == '') {
+        document.getElementById('error').innerHTML = "Please Enter The Task";
+    }
+    //check if text box is not empty
+    else {
+
+        var open = document.getElementById('open');
+        var divTaskAndDiscr = document.createElement('div');
+        divTaskAndDiscr.className = 'contener'
+        divTaskAndDiscr.draggable = 'true';
+
+        let isclick = true;
+        divTaskAndDiscr.addEventListener('click', (e) => {
+            if (isclick) {
+                openTextBox();
+                isclick = false;
+            }
+        })
+
+
+        //click on div tag then open textarea for edit discription
+        function openTextBox() {
+            var textArea = document.createElement('textarea');
+            p.appendChild(textArea)
+            let addDiscriptionBtn = document.createElement('button');
+             addDiscriptionBtn.appendChild(document.createTextNode("Add Discription"));
+            // addDiscriptionBtn.append('<i class="fa fa-times" aria-hidden="true"></i>');
+            p.appendChild(addDiscriptionBtn);
+
+
+
+            //add Discription for Task
+            addDiscriptionBtn.addEventListener('click', (e) => {
+                addDiscription();
+            })
+            function addDiscription() {
+                let textValue = textArea.value;
+                if (textValue == '')
+                 {
+                    // Display an error message or do nothing
+                 } 
+                else
+                   {
+                    // Create a new element to display the text
+                    let textNode = document.createTextNode(textValue);
+                    p.appendChild(textNode);
+                    divTaskAndDiscr.appendChild(p);
+                    textArea.style.display = "none";
+                    addDiscriptionBtn.style.display = "none";
+                   }
+            }
+        }
+
+        var h3 = document.createElement('h3');
+        h3.appendChild(document.createTextNode(text));
+        var p = document.createElement('p');
+        divTaskAndDiscr.appendChild(h3);
+        divTaskAndDiscr.appendChild(p);
+        open.appendChild(divTaskAndDiscr);
+
+
+       //Drag and Drop code
+
+       let draggedItem = null;
+
+
+       divTaskAndDiscr.addEventListener('dragstart', (e) => {
+           console.log("Drag start has been Trigered");
+           draggedItem=divTaskAndDiscr;
+           setTimeout(() => {
+               // e.target.className = "hide";
+               divTaskAndDiscr.style.display="none";
+
+           }, 0);
+
+        })
+        divTaskAndDiscr.addEventListener('dragend', (e) => {
+           console.log("Drag end has been Trigered");
+           draggedItem.style.display ="flex";
+           draggedItem=null;
+           // e.target.className = 'contener';
+        })
+
+         for (box of boxes) {
+
+           box.addEventListener('dragover', (e) => {
+               console.log("Dragover has been trigered");
+               e.preventDefault();
+           })
+
+           box.addEventListener('dragenter', (e) => {
+               console.log("Dragenter has been trigered");
+               e.preventDefault();
+           })
+
+           box.addEventListener('dragleave', (e) => {
+               console.log("Dragleave has been trigered");
+               e.preventDefault();
+           })
+
+           box.addEventListener('drop', (e) => {
+               console.log("Drop has been trigered");
+               e.preventDefault();
+               // e.target.append(divTaskAndDiscr);
+               e.target.appendChild(draggedItem);
+
+           })
+
+        }
+
+    }
+    console.log("Clicked");
 }
-
-function dragEnd() {
-  draggableTodo = null;
-  setTimeout(() => {
-    this.style.display = "block";
-  }, 0);
-  console.log("dragEnd");
-}
-
-all_status.forEach((status) => {
-  status.addEventListener("dragover", dragOver);
-  status.addEventListener("dragenter", dragEnter);
-  status.addEventListener("dragleave", dragLeave);
-  status.addEventListener("drop", dragDrop);
-});
-
-function dragOver(e) {
-  e.preventDefault();
-  //   console.log("dragOver");
-}
-
-function dragEnter() {
-  this.style.border = "1px dashed #ccc";
-  console.log("dragEnter");
-}
-
-function dragLeave() {
-  this.style.border = "none";
-  console.log("dragLeave");
-}
-
-function dragDrop() {
-  this.style.border = "none";
-  this.appendChild(draggableTodo);
-  console.log("dropped");
-}
-
-/* modal */
-const btns = document.querySelectorAll("[data-target-modal]");
-const close_modals = document.querySelectorAll(".close-modal");
-const overlay = document.getElementById("overlay");
-
-btns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    document.querySelector(btn.dataset.targetModal).classList.add("active");
-    overlay.classList.add("active");
-  });
-});
-
-close_modals.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const modal = btn.closest(".modal");
-    modal.classList.remove("active");
-    overlay.classList.remove("active");
-  });
-});
-
-window.onclick = (event) => {
-  if (event.target == overlay) {
-    const modals = document.querySelectorAll(".modal");
-    modals.forEach((modal) => modal.classList.remove("active"));
-    overlay.classList.remove("active");
-  }
-};
-
-/* create todo  */
-const todo_submit = document.getElementById("todo_submit");
-
-todo_submit.addEventListener("click", createTodo);
-
-function createTodo() {
-  const todo_div = document.createElement("div");
-  const input_val = document.getElementById("todo_input").value;
-  const txt = document.createTextNode(input_val);
-
-  todo_div.appendChild(txt);
-  todo_div.classList.add("todo");
-  todo_div.setAttribute("draggable", "true");
-
-  /* create span */
-  const span = document.createElement("span");
-  const span_txt = document.createTextNode("\u00D7");
-  span.classList.add("close");
-  span.appendChild(span_txt);
-
-  todo_div.appendChild(span);
-
-  no_status.appendChild(todo_div);
-
-  span.addEventListener("click", () => {
-    span.parentElement.style.display = "none";
-  });
-  //   console.log(todo_div);
-
-  todo_div.addEventListener("dragstart", dragStart);
-  todo_div.addEventListener("dragend", dragEnd);
-
-  document.getElementById("todo_input").value = "";
-  todo_form.classList.remove("active");
-  overlay.classList.remove("active");
-}
-
-const close_btns = document.querySelectorAll(".close");
-
-close_btns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    btn.parentElement.style.display = "none";
-  });
-});
